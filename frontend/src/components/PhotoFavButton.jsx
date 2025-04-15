c62ed2ea1114a943dd5ec34e10ec7033d638bd46
CodeJuuun <Chomseng.a@gmail.com>
2025-04-15 17:01:56 -0400

    renamed and configured handleClick based on feedback

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,17 +4,17 @@ import '../styles/PhotoFavButton.scss';
 import FavIcon from './FavIcon';
 
 
-const PhotoFavButton = (props) => {
-  const [select, setSelect]= useState(false)
+const PhotoFavButton = () => {
+  const [favouritePhoto, setFavouritePhoto]= useState(false)
 
   const handleClick = () => {
-    setSelect(!select)
+    setFavouritePhoto(prevState => !prevState)
   }
 
   return (
     <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg" onClick={handleClick}>
-        <FavIcon selected={select}/>
+        <FavIcon selected={favouritePhoto}/>
       </div>
     </div>
   );
