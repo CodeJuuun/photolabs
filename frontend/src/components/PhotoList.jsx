54d359d805dbff1d0bfcad1d57b44926d8460c26
CodeJuuun <Chomseng.a@gmail.com>
2025-04-19 14:38:30 -0400

    imported photos from mocks.js and displaying it on page

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
-
+import photos from "../mocks/photos"
 
 const PhotoList = () => {
   return (
     <ul className="photo-list">
-      {sampleDataForPhotoList.map(photo => (
+      {photos.map(photo => (
         <PhotoListItem
           key={photo.id}
           photo={{
