09020ac76c0ea166fc57b918090a6e3cd38d2e5e
CodeJuuun <Chomseng.a@gmail.com>
2025-04-19 14:50:26 -0400

    imported topics and photos from mock and passed in as props to App.jsx. Modified TopNavigationBar to accept topics as props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,17 @@ import PhotoList from './components/PhotoList';
 import TopicList from './components/TopicList';
 import TopNavigation from './components/TopNavigationBar';
 import HomeRoute from './routes/HomeRoute';
+import topics from './mocks/topics';
+import photos from './mocks/photos';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute
+        photos={photos}
+        topics={topics} />
     </div>
   );
 };
