d9ed8b919afc001dc3ae3e7ac47607d164e664bd
CodeJuuun <Chomseng.a@gmail.com>
2025-04-21 18:37:39 -0400

    started close modal aspect but buggy

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,14 +1,23 @@
-import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ closeModal, photo }) => {
   return (
-    <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
-    </div>
-  )
+    <>
+      <div className="photo-details-modal">
+        <button className="photo-details-modal__close-button" onClick={closeModal}>
+          <img src={closeSymbol} alt="close symbol" />
+          {console.log(photo)}
+        </button>
+      </div>
+      <div className='photo-details-modal__image'>
+        <img src={photo} />
+      </div>
+      <div className='photo-details-modal__images'>
+
+      </div>
+    </>
+  );
 };
 
 export default PhotoDetailsModal;
