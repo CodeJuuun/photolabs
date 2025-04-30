7894f1631f15d37b9c7b0f0f05ebdb59529b0644
CodeJuuun <Chomseng.a@gmail.com>
2025-04-30 15:49:14 -0400

    passed in handleClick

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,8 +1,15 @@
 import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
+
+  const handleClick = () => {
+    props.onClick(props.id);
+  };
+
   return (
-    <div className="topic-list__item">
+    <div className="topic-list__item"
+         onClick={handleClick}
+    >
       <span>{props.title}</span>
     </div>
   );
