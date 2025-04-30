086d9dd2029af4c8f119a6f09ef1b126991f8c23
CodeJuuun <Chomseng.a@gmail.com>
2025-04-30 15:47:49 -0400

    passed in Id of topicData prop to TopicList

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,11 +2,15 @@ import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
 
-const TopicList = ({ topics }) => {
+const TopicList = ({ topics, handleClick }) => {
   return (
     <div className="top-nav-bar__topic-list">
       {topics.map((topicData) => (
-        <TopicListItem key={topicData.id} title={topicData.title} />
+        <TopicListItem 
+        key={topicData.id} 
+        id={topicData.id}
+        title={topicData.title}
+        onClick={handleClick} />
       ))}
     </div>
   );
