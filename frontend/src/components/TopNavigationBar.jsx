c708a78409acb99808a180417fb506d547d81a38
CodeJuuun <Chomseng.a@gmail.com>
2025-04-30 15:21:17 -0400

    created handle click to call on onLoadTopic when clicked

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,12 +1,17 @@
-import '../styles/TopNavigationBar.scss'
+import '../styles/TopNavigationBar.scss';
 import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 
 const TopNavigation = ({ topics, onLoadTopic, isFavPhotoExist }) => {
+
+  const handleClick = (topicId) => {
+    onLoadTopic(topicId);
+  };
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics} />
+      <TopicList topics={topics} handleClick={handleClick} />
       <FavBadge isFavPhotoExist={isFavPhotoExist} />
     </div>
   );
