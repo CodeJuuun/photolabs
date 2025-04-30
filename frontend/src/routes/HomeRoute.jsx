b3b1e029d75a1c3957ada0fff825476b8a2410b0
CodeJuuun <Chomseng.a@gmail.com>
2025-04-30 14:56:39 -0400

    removed console log from HomeRoute

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,8 +3,7 @@ import TopNavigation from '../components/TopNavigationBar';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = ({ photos, topics, likedPhotos, toggleFavourite, onPhotoSelect }) => {
-  console.log( "rendering photos in homeroute successful")
-  console.log( "rendering topics in homeroute successful")
+
   return (
     <div className="home-route">
       <TopNavigation topics={topics} isFavPhotoExist={likedPhotos.length > 0} />
